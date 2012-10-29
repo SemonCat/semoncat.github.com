ca7f8638fca36708b9bb7cacd7e7bddb4993f607
SemonCat <SemonCat@gmail.com>
2012-10-29 22:40:35 +0800

    Site updated at 2012-10-29 14:40:35 UTC

--- javascripts/autoblank.js
+++ javascripts/autoblank.js
@@ -1,13 +1,11 @@
 $(document).ready(function() {
-    $('pre').addClass('prettyprint linenums') //添加Google code Hight需要的class
-    // 這段代碼是用來提取文章中鏈接到其他網站的鏈接
-    // 然後給這寫鏈接加上外鏈的樣式，並且修改為在新窗口打開(target=_blank)
+    $('pre').addClass('prettyprint linenums');
     $('.entry a').each(function(index, element) {
         var href = $(this).attr('href');
-        if(href.indexOf('#') == 0) {} else if(href.indexOf('/') == 0 || href.toLowerCase().indexOf('beiyuu.com') > -1) {} else {
+        if(href.indexOf('#') == 0) {} else if(href.indexOf('/') == 0 || href.toLowerCase().indexOf('semoncat.github.com') > -1) {} else {
             $(this).attr('target', '_blank');
             $(this).addClass('external');
         }
-    BYB.includeScript('/js/prettify/prettify.js',function(){prettyPrint()});
     });
+    prettyPrint();
 });
